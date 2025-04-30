ffe7373877fb47f702068a7cb8703f8d45c64267
Yi-Jui Wu <herbage@redcoat.ai>
2025-04-30 12:19:44 +0800

    optimize structures

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -10,7 +10,7 @@ const NavBar = () => {
   const currentPath = usePathname();
   const Links = [
     { href: "/", label: "Dashboard" },
-    { href: "/issues", label: "Issues" },
+    { href: "/issues/list", label: "Issues" },
   ];
 
   return (
--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -14,7 +14,7 @@ const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
     try {
       setIsDeleting(true);
       await axios.delete(`/api/issues/${issueId}`);
-      router.push("/issues");
+      router.push("/issues/list");
       router.refresh();
     } catch (error) {
       console.error("Error deleting issue:", error);
