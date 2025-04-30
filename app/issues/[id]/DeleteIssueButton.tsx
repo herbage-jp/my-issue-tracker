a3ba37ee724cc9e48d620421903a2c0fa4d12ba0
Yi-Jui Wu <herbage@redcoat.ai>
2025-04-30 11:58:45 +0800

    show a confirmation dialog

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,17 +1,31 @@
+"use client";
 import { Pencil2Icon } from "@radix-ui/react-icons";
-import { Button } from "@radix-ui/themes";
+import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 import Link from "next/link";
 import React from "react";
 
-const EditIssueButton = ({ issueId }: { issueId: number }) => {
+const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
   return (
-    <Button asChild color="red">
-      <Link href={`/issues/${issueId}/edit`}>
-        <Pencil2Icon />
-        <span>Edit Issue</span>
-      </Link>
-    </Button>
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color="red"> Delete Issue</Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content>
+        <AlertDialog.Title>Delete Issue</AlertDialog.Title>
+        <AlertDialog.Description>
+          Are you sure you want to delete this issue?
+        </AlertDialog.Description>
+        <Flex className="mt-4" gap="2">
+          <AlertDialog.Cancel>
+            <Button variant="soft">Cancel</Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button color="red">Delete</Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
   );
 };
 
-export default EditIssueButton;
+export default DeleteIssueButton;
