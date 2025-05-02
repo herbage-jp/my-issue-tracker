c63d9cbdbcb645b00a029599c8044ebb40b7efa0
Yi-Jui Wu <herbage@redcoat.ai>
2025-05-02 12:22:29 +0800

    build the latest issues component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
-import Pagination from "@/app/components/Pagination";
+import LatestIssues from "./issues/LatestIssues";
 
-export default async function Home({
-  searchParams,
-}: {
-  searchParams: { page?: string };
-}) {
-  const resolvedParams = await searchParams;
-  const page = parseInt(resolvedParams.page || "1");
-  return <Pagination itemCount={100} pageSize={10} currentPage={page} />;
+export default async function Home() {
+  return <LatestIssues />;
 }
