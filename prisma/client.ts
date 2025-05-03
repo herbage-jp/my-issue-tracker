95874aca6bc7d33e626017d2b8bda0076eafe78d
Yi-Jui Wu <herbage@redcoat.ai>
2025-05-03 16:46:36 +0800

    fix prisma initialization bug

--- prisma/client.ts
+++ prisma/client.ts
@@ -6,4 +6,5 @@ const globalForPrisma = global as unknown as { prisma: PrismaClient };
 export const prisma =
   globalForPrisma.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+// Always cache the Prisma instance in both development and production
+globalForPrisma.prisma = prisma;
